5feea32ca91faa11f8c187f5a7a0d567da4f2afc
sg0703 <samgates@Sams-MacBook-Pro.local>
2021-06-06 20:55:07 -0400

    Updated weather.js

--- src/apis/weather.js
+++ src/apis/weather.js
@@ -2,7 +2,16 @@ import axios from 'axios';
 import { KEY } from './config.js'; 
 // file that exports const KEY w/ OpenWeather API key
 
-require('dotenv').config({ path: __dirname + '/../.env'});
+/** 
+ * 
+ * YOU WILL NEED TO CREATE YOUR OWN OPENWEATHER API KEY FOR THIS TO WORK
+ *  
+ * TWO OPTIONS: 
+ * 1. STORE THE KEY BELOW OR IN A VARIABLE WITHIN THIS FILE (THEN DELETE THE IMPORT STATEMENT ON LINE 2)
+ * 2. WRITE YOUR OWN CONFIG.JS FILE THAT EXPORTS A SINGLE KEY VARIABLE WITH YOUR API KEY
+ * 
+ * 
+ * */
 
 export default axios.create({
     baseURL: 'https://api.openweathermap.org/data/2.5/',
