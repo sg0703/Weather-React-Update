0fbff4060e751fe815f3832658d90b7d35c0e247
sg0703 <samgates@Sams-MBP.attlocal.net>
2021-06-09 11:23:45 -0400

    Switched to using environment variable for key, deleted config

--- src/apis/config.js
+++
@@ -1,3 +0,0 @@
-// file to hide API key
-
-export const KEY = '7cb72a69de59a3be2cba0e378b88098f';
--- src/apis/weather.js
+++ src/apis/weather.js
@@ -1,21 +1,9 @@
 import axios from 'axios';
-import { KEY } from './config.js'; 
-// file that exports const KEY w/ OpenWeather API key
 
-/** 
- * 
- * YOU WILL NEED TO CREATE YOUR OWN OPENWEATHER API KEY FOR THIS TO WORK
- *  
- * TWO OPTIONS: 
- * 1. STORE THE KEY BELOW OR IN A VARIABLE WITHIN THIS FILE (THEN DELETE THE IMPORT STATEMENT ON LINE 2)
- * 2. WRITE YOUR OWN CONFIG.JS FILE THAT EXPORTS A SINGLE KEY VARIABLE WITH YOUR API KEY
- * 
- * 
- * */
-
+// api key stored in .env file
 export default axios.create({
     baseURL: 'https://api.openweathermap.org/',
     params: {
-        appid: KEY
+        appid: process.env.REACT_APP_API_KEY
     }
 });
